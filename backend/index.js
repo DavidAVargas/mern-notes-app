a5fc7a5a6e651019280a8a9678351fc465dc5114
David A. Vargas <davidavargas.dev@gmail.com>
2024-12-26 15:55:49 -0600

    update added

--- backend/index.js
+++ backend/index.js
@@ -221,6 +221,35 @@ app.get("/delete-note/:noteID", authenticateToken, async (req,res) =>{
     }
 });
 
+// Update
+app.put("/update-note/:noteID", authenticateToken, async (req,res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+
+    if (!title && !content && !tags) {
+        return res.status(400).json({ error: true, message: 'At least one field is required' });
+    }
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+        if (!note) {
+            return res.status(404).json({ error: true, message: 'Note not found' });
+        }
+
+        if (isPinned) 
+            note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({ error: false, note, message: 'Note updated successfully' });
+
+    } catch (error) {
+        return res.status(500).json({ error: true, message: 'Internal server error' });
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
